bbd2b6d00fd1e5a272f6fd7617cffaa80aba97c1
Adam Cottis <adam.cottis@seccl.tech>
2025-03-24 10:46:42 +0000

    Add vuln

--- hello.ts
+++ hello.ts
@@ -1,2 +1,7 @@
 
 console.log("Hello, World!");
+
+const userInput = req.query.user;
+const query = "SELECT * FROM users WHERE name = '" + userInput + "'"; // SQL Injection vulnerability
+db.execute(query);
+
